eec2e37a5614070e78b60a4478cc58877b2a61d6
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-11-29 13:10:25 +0200

    Simplify favicon directory placement

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,9 +18,9 @@ class MyDocument extends Document {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
